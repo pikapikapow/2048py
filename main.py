e1bce7176eb7ad03e8f97bfc847bbb2147800e96
pikapikapow <vladimirhwang84@gmail.com>
2024-06-18 12:34:03 -0400

    Made tiles, one not showing up

--- main.py
+++ main.py
@@ -78,8 +78,11 @@ def draw_grid(window):
 
     pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
 
-def draw(window):
+def draw(window, tiles):
     window.fill(BACKGROUND_COLOR)
+
+    for tile in tiles.values():
+        tile.draw(window)
 
     draw_grid(window)
 
@@ -88,6 +91,8 @@ def draw(window):
 def main(window):
     clock = pygame.time.Clock()
     run = True
+
+    tiles = {"00": Tile(4, 0, 0), "20": Tile(128, 2, 0)}
     
     while run:
         clock.tick(FPS)
@@ -97,7 +102,7 @@ def main(window):
                 run = False
                 break
         
-        draw(window)
+        draw(window, tiles)
 
     pygame.quit()
 
